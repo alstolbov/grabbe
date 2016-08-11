20506ba9bcd97d1796195acdf5ad0e8e9f2a86cd
stolbov <stolbov@informatica.ru>
2016-08-11 16:31:57 +0300

    with server client

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ var express = require('express');
 
 var app = express();
 var port = 8080;
+// var bodyParser = require("body-parser");
+//
+// app.use(bodyParser.urlencoded({
+//   extended: true
+// }));
 
 
 var logFile = fs.createWriteStream('log.txt', { flags: 'w+' });
@@ -93,80 +98,88 @@ var getAllParents = function ($, _class, parentsArr) {
 };
 
 app.get('/', function (_req, _res) {
-  request
-    .get('http://lenta.ru')
-    .end(function (err, res) {
-      if (!err) {
-        var $ = cheerio.load(res.text, {decodeEntities: false});
+  if (!_req.query.parse || _req.query.parse == "") {
+    _res.send('ready');
+  } else {
+    request
+      .get(_req.query.parse)
+      .end(function (err, res) {
+        if (!err) {
+          var $ = cheerio.load(res.text, {decodeEntities: false});
 
-        // console.log($('body').html());
-        var bodyArray = $('body').toArray()[0];
-        // getNodes(bodyArray);
-        var tagList = buildNodeList(bodyArray);
+          // console.log($('body').html());
+          var bodyArray = $('body').toArray()[0];
+          // getNodes(bodyArray);
+          var tagList = buildNodeList(bodyArray);
 
-        var nodes = getMaxUsageNodes(tagList);
-        var middle = 0;
-        nodes.map(function (tag) {
-          middle += tag.count;
-        });
-        middle = middle/nodes.length;
-        var suspectNodes = [];
-        nodes.map(function (node) {
-          if(node.count > middle && node.parent) {
-            node.parentCount = getItem({
-              in: nodes,
-              where: 'class',
-              eq: node.parent
-            }).count;
-            node.diff = parseInt(node.parentCount*100/node.count);
-            if (node.diff < 50) {
-              node.isSuspect = true;
-              suspectNodes.push(node);
+          var nodes = getMaxUsageNodes(tagList);
+          var middle = 0;
+          nodes.map(function (tag) {
+            middle += tag.count;
+          });
+          middle = middle/nodes.length;
+          var suspectNodes = [];
+          nodes.map(function (node) {
+            if(node.count > middle && node.parent) {
+              node.parentCount = getItem({
+                in: nodes,
+                where: 'class',
+                eq: node.parent
+              }).count;
+              node.diff = parseInt(node.parentCount*100/node.count);
+              if (node.diff < 50) {
+                node.isSuspect = true;
+                suspectNodes.push(node);
+              }
             }
-          }
-        });
+          });
 
-        // nodes.sort(function (a, b) {
-        //     return b.diff - a.diff;
-        // });
-        var maxDiff = 0;
-        var maxContent = 0;
-        suspectNodes.map(function (tag) {
-          var content = $('.' + tag.class.split(' ').join('.')).text();
-          tag.content = content.length;
-          maxContent += tag.content;
-          maxDiff += tag.diff;
-          // tag.allParents = getAllParents($, tag.class, []);
-        });
+          // nodes.sort(function (a, b) {
+          //     return b.diff - a.diff;
+          // });
+          var maxDiff = 0;
+          var maxContent = 0;
+          suspectNodes.map(function (tag) {
+            var content = $('.' + tag.class.split(' ').join('.')).text();
+            tag.content = content.length;
+            maxContent += tag.content;
+            maxDiff += tag.diff;
+            // tag.allParents = getAllParents($, tag.class, []);
+          });
 
-        var HTML = '';
-        nodes.map(function (tag) {
-          if (tag.diff < maxDiff/suspectNodes.length && tag.content) {
-            logger(tag.class, tag.count, tag.parent, tag.parentCount, tag.diff + '%', tag.content);
-            var tmpText = 'From: ' + tag.class +
-              '<br>count:' + tag.count +
-              '<br>parent:' + tag.parent +
-              '<br>parentCount:' + tag.parentCount +
-              '<br>diff:' + tag.diff + '%' +
-              '<br>content:' + tag.content
-            ;
-            // logger(tag.allParents);
-            // logger();
-            HTML += '<div style="margin: 20px; padding: 10px; border: 1px solid #ddd;"><div style="background: #ddd; font-weight: bold;">' +
-              tmpText +
-              '</div>';
-            $('.' + tag.class.split(' ').join('.')).each(function (i, el) {
-              HTML += $(this).html();
-              HTML += '<hr>';
+          var HTML = '';
+          if (!suspectNodes.length) {
+            // HTML += 'nothing';
+            HTML += $('body').html();
+          } else {
+            suspectNodes.map(function (tag) {
+              if (tag.diff < maxDiff/suspectNodes.length && tag.content) {
+                logger(tag.class, tag.count, tag.parent, tag.parentCount, tag.diff + '%', tag.content);
+                var tmpText = 'From: ' + tag.class +
+                  '<br>count:' + tag.count +
+                  '<br>parent:' + tag.parent +
+                  '<br>parentCount:' + tag.parentCount +
+                  '<br>diff:' + tag.diff + '%' +
+                  '<br>content:' + tag.content
+                ;
+                // logger(tag.allParents);
+                // logger();
+                HTML += '<div style="margin: 20px; padding: 10px; border: 1px solid #ddd;"><div style="background: #ddd; font-weight: bold;">' +
+                  tmpText +
+                  '</div>';
+                $('.' + tag.class.split(' ').join('.')).each(function (i, el) {
+                  HTML += $(this).html();
+                  HTML += '<hr>';
+                });
+                HTML += '</div>'
+              }
             });
-            HTML += '</div>'
           }
-        });
-
-        _res.send(HTML);
-      }
-    })
-  ;
+          _res.send(HTML);
+        }
+      })
+    ;
+  }
 });
 
 app.listen(port, function () {
